ddbfbb92c192afe27411e77d6325a2484e6b7fd0
DuoB Coder <duobcoder@gmail.com>
2020-05-09 17:26:10 +0530

    Update Health Check

--- app.js
+++ app.js
@@ -19,8 +19,10 @@ var secret = require('dvp-common/Authentication/Secret.js');
 var authorization = require('dvp-common/Authentication/Authorization.js');
 //var tftp = require('tftp');
 var fs = require("fs");
-
-
+var healthcheck = require('dvp-healthcheck/DBHealthChecker');
+var mongomodels = require('dvp-mongomodels');
+var dbModel = require('dvp-dbmodels');
+var {redisClient} = require('./RedisHandler');
 
 var port = config.Host.port || 3000;
 var version=config.Host.version;
@@ -45,121 +47,14 @@ var RestServer = restify.createServer({
 restify.CORS.ALLOW_HEADERS.push('authorization');
 RestServer.use(restify.CORS());
 RestServer.use(restify.fullResponse());
-RestServer.use(jwt({secret: secret.Secret}));
+RestServer.use(jwt({secret: secret.Secret}).unless({path: ['/healthcheck']}));
 RestServer.use(restify.bodyParser());
 RestServer.use(restify.acceptParser(RestServer.acceptable));
 RestServer.use(restify.queryParser());
 RestServer.use(cors());
 
-var mongoip=config.Mongo.ip;
-var mongoport=config.Mongo.port;
-var mongodb=config.Mongo.dbname;
-var mongouser=config.Mongo.user;
-var mongopass = config.Mongo.password;
-var mongoreplicaset= config.Mongo.replicaset;
-
-var mongoose = require('mongoose');
-var connectionstring = '';
-mongoip = mongoip.split(',');
-
-if(util.isArray(mongoip)){
-    if(mongoip.length > 1){
-        mongoip.forEach(function(item){
-            connectionstring += util.format('%s:%d,',item,mongoport)
-        });
-
-        connectionstring = connectionstring.substring(0, connectionstring.length - 1);
-        connectionstring = util.format('mongodb://%s:%s@%s/%s',mongouser,mongopass,connectionstring,mongodb);
-
-        if(mongoreplicaset){
-            connectionstring = util.format('%s?replicaSet=%s',connectionstring,mongoreplicaset) ;
-        }
-    }
-    else
-    {
-        connectionstring = util.format('mongodb://%s:%s@%s:%d/%s',mongouser,mongopass,mongoip[0],mongoport,mongodb);
-    }
-}else{
-
-    connectionstring = util.format('mongodb://%s:%s@%s:%d/%s',mongouser,mongopass,mongoip,mongoport,mongodb);
-}
-
-
-console.log("ConnectionString ------------ "+connectionstring);
-
-mongoose.connect(connectionstring,{server:{auto_reconnect:true}});
-
-
-mongoose.connection.on('error', function (err) {
-    console.error( new Error(err));
-    mongoose.disconnect();
-
-});
-
-mongoose.connection.on('opening', function() {
-    console.log("reconnecting... %d", mongoose.connection.readyState);
-});
-
-
-mongoose.connection.on('disconnected', function() {
-    console.error( new Error('Could not connect to database'));
-    mongoose.connect(connectionstring,{server:{auto_reconnect:true}});
-});
-
-mongoose.connection.once('open', function() {
-    console.log("Connected to db");
-
-});
-
-
-mongoose.connection.on('reconnected', function () {
-    console.log('MongoDB reconnected!');
-});
-
-
-
-process.on('SIGINT', function() {
-    mongoose.connection.close(function () {
-        console.log('Mongoose default connection disconnected through app termination');
-        process.exit(0);
-    });
-});
-
-
-
-
-var encryptPass = config.Host.encryptionPassword;
-
-function encryptTest(text)
-{
-    try{
-        var key = '00000000000000000000000000000000'; //replace with your key
-        var iv = '0000000000000000'; //replace with your IV
-
-        var cipher = crypto.createCipheriv('aes256', key, iv);
-        var crypted = cipher.update("DuoS123456789", 'utf8', 'base64');
-        crypted += cipher.final('base64');
-        console.log(crypted);
-
-
-        var decipher = crypto.createDecipher('aes256', key,iv);
-        var decrypted = decipher.update(crypted, 'utf8', 'base64');
-        decrypted += decipher.final('base64');
-        console.log(decrypted);
-
-        return crypted;
-    }
-    catch (ex){
-        console.log(ex)
-    }
-
-
-
-    /*var ciphertext = CryptoJS.AES.encrypt(text, encryptPass);
-    return ciphertext.toString();*/
-}
-
-
+var hc = new healthcheck(RestServer, {redis: redisClient, pg: dbModel.SequelizeConn, mongo:mongomodels.connection });
+hc.Initiate();
 
 RestServer.post('/DVP/API/:version/SipUser/DidNumber', authorization({resource:"number", action:"write"}), function(req, res, next) {
     var reqId = uuid.v1();
